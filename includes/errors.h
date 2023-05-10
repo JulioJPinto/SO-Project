0808d0ad9170a61be60970df46a9b3d2d805212a
Júlio Pinto <pinto.julioj03@gmail.com>
2023-05-10 20:51:05 +0100

    refactor: remove size to errorinfo

--- includes/errors.h
+++ includes/errors.h
@@ -16,27 +16,20 @@ typedef enum {
     TOO_MANY_PIDS
 } error;
 
-typedef struct ErrorInfo {
-    char *message;
-    size_t size;
-} ErrorInfo;
+typedef char* ErrorInfo ;
 
 static const ErrorInfo errors[] = {
-    // There is no error info for 'NONE' being its the default value for
-    // veryfing
-    {"Program usage: ./tracer <command> (options) (arguments)\n",
-     sizeof(char) * 57},
-    {"Unknown command\n", sizeof(char) * 17},
-    {"Unknown option\n", sizeof(char) * 16},
-    {"Error creating FIFO\n", sizeof(char) * 21},
-    {"Couldn't find the original request upon the program finishing\n",
-     sizeof(char) * 63},
-    {"Requested program exited with errors\n", sizeof(char) * 38},
-    {"Program usage: ./monitor <Process folder path>\n", sizeof(char) * 48},
-    {"No PIDS given\n", sizeof(char) * 15},
-    {"File not found\n", sizeof(char) * 16},
-    {"Too many PIDs passed as argument, increase NAME_MAX constant\n", sizeof(char) * 62},
-
+    // There is no error info for 'NONE' being its the default value for veryfing
+    "Program usage: ./tracer <command> (options) (arguments)\n",
+    "Unknown command\n",
+    "Unknown option\n",
+    "Error creating FIFO\n",
+    "Couldn't find the original request upon the program finishing\n",
+    "Requested program exited with errors\n",
+    "Program usage: ./monitor <Process folder path>\n",
+    "No PIDS given\n",
+    "File not found\n",
+    "Too many PIDs passed as argument, increase NAME_MAX constant\n",
 };
 
 int print_error(error error);
--- src/errors.c
+++ src/errors.c
@@ -1,7 +1,8 @@
 #include <unistd.h>
+#include <string.h>
 
 #include "errors.h"
 
 int print_error(error error) {
-    return write(STDERR_FILENO, errors[error - 1].message, errors[error - 1].size);
+    return write(STDERR_FILENO, errors[error - 1], sizeof(char) * strlen(errors[error - 1]));
 }
